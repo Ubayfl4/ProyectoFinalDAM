3894204481d924847515d0aa76943a8d9e079b60
Ubayfl4 <ubayfl4@gmail.com>
2024-03-19 12:40:29 +0000

    menú desplegable a mitad
    no funciona bien

--- app/src/main/java/com/example/proyectofinalcinemalist/MyApp.kt
+++ app/src/main/java/com/example/proyectofinalcinemalist/MyApp.kt
@@ -10,20 +10,24 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -31,6 +35,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
 
 
 enum class Pantallas(){
@@ -46,6 +51,11 @@ fun MyApp(
     navController: NavHostController = rememberNavController()
 ){
     var showTopBar by remember { mutableStateOf(false) }
+    ModalNavigationDrawer(
+        drawerContent = { NavigationDrawer() }
+    ){
+
+    }
     Scaffold(
         topBar = {
             if (showTopBar) {
@@ -96,6 +106,23 @@ fun MyApp(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBarCinemalist(){
+    val botones = listOf("Buscador", "Perfil", "Inicio", "Listas", "Cerrar seción")
+    val scope = rememberCoroutineScope()
+    val estadoDrawer = rememberDrawerState(DrawerValue.Closed)
+    val botonSeleccionado = remember { mutableStateOf(botones[0])}
+
+    ModalDrawerSheet {
+        botones.forEach{ boton ->
+            NavigationDrawerItem(
+                label = { Text(text = boton) },
+                selected = boton.equals(botonSeleccionado),
+                onClick = {
+                scope.launch { estadoDrawer.close() }
+                botonSeleccionado.value = boton
+                }
+            )
+        }
+    }
     CenterAlignedTopAppBar(
         title = { Image(painter = painterResource(id = R.drawable.logocinemalistblanco), contentDescription ="logo" ) },
         colors = TopAppBarDefaults.topAppBarColors(
@@ -103,7 +130,7 @@ fun TopBarCinemalist(){
             titleContentColor = MaterialTheme.colorScheme.primary,
         ),
         navigationIcon = {
-            IconButton(onClick = { /* */ }) {
+            IconButton(onClick = { scope.launch { estadoDrawer.open() } }) {
                 Icon(
                     imageVector = Icons.Filled.Menu,
                     contentDescription = "menu"
@@ -121,16 +148,20 @@ fun TopBarCinemalist(){
 @Composable
 fun NavigationDrawer(){
     val botones = listOf("Buscador", "Perfil", "Inicio", "Listas", "Cerrar seción")
-    Column {
-        botones.forEach{ boton ->
-            TextButton(onClick = { /*TODO*/ }) {
-                Text(text = boton)
-            }
-        }
-    }
+    val scope = rememberCoroutineScope()
+    val estadoDrawer = rememberDrawerState(DrawerValue.Closed)
+    val botonSeleccionado = remember { mutableStateOf(botones[0])}
+
     ModalDrawerSheet {
         botones.forEach{ boton ->
-            NavigationDrawerItem(label = { Text(text = boton) }, selected = , onClick = { /*TODO*/ })
+            NavigationDrawerItem(
+                label = { Text(text = boton) },
+                selected = boton.equals(botonSeleccionado),
+                onClick = {
+                    scope.launch { estadoDrawer.close() }
+                    botonSeleccionado.value = boton
+                }
+            )
         }
     }
 }
